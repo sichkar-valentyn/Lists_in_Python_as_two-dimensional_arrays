7040b46a098f0ce728f0dc6cd93f5d9dc521b50d
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-03-29 14:57:01 +0300

    Update Lists_in_Python_as_two-dimensional_arrays.py

--- Lists_in_Python_as_two-dimensional_arrays.py
+++ Lists_in_Python_as_two-dimensional_arrays.py
@@ -1,3 +1,14 @@
+# File: Lists_in_Python_as_two-dimensional_arrays.py
+# Description: Examples how to create 2-dimensional lists in Python and use them as arrays
+# Environment: PyCharm and Anaconda environment
+#
+# MIT License
+# Copyright (c) 2018 Valentyn N Sichkar
+# github.com/sichkar-valentyn
+#
+# Reference to:
+# [1] Valentyn N Sichkar. Lists in Python as two-dimensional arrays // GitHub platform [Electronic resource]. URL: https://github.com/sichkar-valentyn/Lists_in_Python_as_two-dimensional_arrays (date of access: XX.XX.XXXX)
+
 m = 'end'
 a = []
 b = []
